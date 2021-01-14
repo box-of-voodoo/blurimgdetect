9e450879f3cc5e4f5bf8bfadfe480b0ddcfa7b14
Box Voodoo <boxofvoodoo@gmail.com>
2021-01-14 21:17:11 +0100

    Time measurment, map -> in procesed folder can be now others files and folders

--- main.cpp
+++ main.cpp
@@ -3,72 +3,75 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
-#include <string>
-#include <Windows.h>
+//#include <string>
 #include <filesystem>
 #include <vector>
+#include <map>
+
+#include <chrono>
 
 using std::cout;
 using std::endl;
 
 int main(/*int argc, char** argv*/)
 {
+
     cv::Mat image, eges;
     cv::Mat m, sd;
     cv::Mat out;// (image.rows, image.cols * image.channels(), CV_64F);
     cv::Mat splitchannels[3];
     
-    std::string path = "d:/1_Kuba/fotky";
+    std::string path = "d:/1_Kuba/fotky/x";
     double suma = 0;
     double maxVal = DBL_MIN;
     double minVal = DBL_MAX;
+    double limit = 0;
+    int count = 0;
     std::vector<double> stds;
+    std::map<std::filesystem::path, double> imagesStd;
     cout << "Processing images..." << endl;
 
-        for (const auto& file : std::filesystem::directory_iterator(path))
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (const auto& file : std::filesystem::directory_iterator(path))
+    {
+
+        image = cv::imread(file.path().string(), cv::IMREAD_COLOR); // Read the file
+
+        if (!image.empty()) // Check for invalid input
         {
-
-            image = cv::imread(file.path().string(), cv::IMREAD_COLOR); // Read the file
-
-            if (image.empty()) // Check for invalid input
-            {
-                std::cout << "Could not open or find the image" << std::endl;
-                return -1;
-            }
-
             cv::Laplacian(image, eges, CV_64F);
             cv::split(eges, splitchannels);
             cv::hconcat(splitchannels, 3, out);
-
-
             cv::meanStdDev(out, m, sd);
-
-            //bool remove(const std::filesystem::path& p);
-            stds.push_back(pow(*(double*)sd.data, 2.));
-            minVal = min(minVal, stds.back());
-            maxVal = max(maxVal, stds.back());
-            suma += stds.back();
-        }
-        double limit = 0;
-        cout << "Number of photos: " << stds.size() << endl;
-        cout << "Max value: " << maxVal << "\tMin value: " << minVal << "\tAverage value: " << suma / stds.size() << endl;
-        cout << "\nRemove under: ";
-        std::cin >> limit;
-        cout << "\nRemoving photos..." << endl;
-
-        int count = 0;
-        for (const auto& file : std::filesystem::directory_iterator(path))
-        {
-            if (stds[count++] < limit) 
-                std::filesystem::remove(file.path());
-        }
             
-        
-        cv::putText(image, cv::String(std::to_string(pow(*(double*)sd.data, 2.))), cv::Point(100, 100), 0, 4, cv::Scalar(0, 0, 255), 5);
-        cv::namedWindow("Display window", cv::WINDOW_NORMAL); // Create a window for display.
-        cv::imshow("Display window", image); // Show our image inside it.
+            double temp = (pow(*(double*)sd.data, 2.));
+            imagesStd[file.path()] = temp;
+            minVal = MIN(minVal, temp);
+            maxVal = MAX(maxVal, temp);
+            suma += temp;
+        }
 
-        cv::waitKey(0); // Wait for a keystroke in the window
+
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    cout << "Time calculating: " << time.count() << " ms" << endl;
     
+
+    cout << "Number of photos: " << imagesStd.size() << endl;
+    cout << "Max value: " << maxVal << "\tMin value: " << minVal << "\tAverage value: " << suma / imagesStd.size() << endl;
+    cout << "Remove under: ";
+    std::cin >> limit;
+    cout << "Removing photos..." << endl;
+
+    for (const auto& [key,val] : imagesStd)
+    {
+        if (val < limit) 
+            count += std::filesystem::remove(key);
+    }
+
+    cout << "Photos removed: " << count << endl;
+       
     return 0;
 }
